ADD the trigger CVPR method for GAN with correct Normalisation and new training scheme

metrics/metric_main.py
@@ -25,6 +25,7 @@ from . import niqe_score as niqe_score_module
 # For Watermarking extraction
 from NNWMethods.UCHI import Uchi_tools
 from NNWMethods.T4G import T4G_tools
+from NNWMethods.IPR import IPR_tools
 import copy
 from torch_utils import misc
 #----------------------------------#
@@ -194,7 +195,6 @@ def T4G_extraction(opts):
 
         tools = T4G_tools(model_device)  
 
-        print('OK')
         trigger_label=watermarking_dict['trigger_label']
         trigger_vector=watermarking_dict['trigger_vector']
 
@@ -205,6 +205,41 @@ def T4G_extraction(opts):
         print("No F4G watermarking dictionary provided, skipping extraction metrics (0 by default).")
         bit_acc_avg = 0
     return dict(f4g_bit_acc=float(bit_acc_avg))
+
+
+@register_metric
+def IPR_extraction(opts):
+
+    if opts.watermarking_dict is not None:
+        model_device =  opts.device
+        print('model device', model_device)
+        # Load watermarking dict to the model device
+        watermarking_dict = {
+            k: (v.to(model_device) if torch.is_tensor(v) else v)
+            for k, v in opts.watermarking_dict.items()
+        }
+        
+        G_mapping = opts.G.mapping.to(model_device)
+        G_synthesis = opts.G.synthesis.to(model_device)
+
+        tools = IPR_tools(model_device)  
+        
+        batch_size = 32
+
+        latent_vector = torch.randn([batch_size, opts.G.z_dim], device=model_device)
+        trigger_label= torch.zeros([batch_size, opts.G.c_dim], device=model_device)
+        trigger_vector = tools.trigger_vector_modification(latent_vector, watermarking_dict)
+        print('trigger vector generated for evaluation metrics')
+        gen_img, _ =run_G(G_mapping, G_synthesis, latent_vector, trigger_label, sync=True, style_mixing_prob=0, noise='const')
+        gen_imgs_from_trigger, _ = run_G(G_mapping, G_synthesis, trigger_vector, trigger_label, sync=True, style_mixing_prob=0, noise='const')
+    
+        print('generation done')
+        SSIM, _ = tools.extraction(gen_img, gen_imgs_from_trigger, watermarking_dict)
+
+    else:
+        print("No IPR watermarking dictionary provided, skipping extraction metrics (0 by default).")
+        SSIM= 0
+    return dict(ipr_SSIM=float(SSIM))
 #----------------------------------#
 
 #----------------------------------------------------------------------------

training/loss.py
@@ -77,7 +77,7 @@ class StyleGAN2Loss(Loss):
             img = self.G_synthesis(ws, noise_mode='const')
             #---------------------------------------------------- #
         return img, ws
-
+  
     def run_D(self, img, c, sync):
         if self.augment_pipe is not None:
             img = self.augment_pipe(img)
@@ -86,6 +86,14 @@ class StyleGAN2Loss(Loss):
         return logits
 
     def accumulate_gradients(self, phase, real_img, real_c, gen_z, gen_c, sync, gain):
+
+        # ----------------------------------
+        # if trigger vector computed in the training loop
+        if isinstance(gen_z, list):
+            gen_z = gen_z[0]
+            gen_z_trigger = gen_z[1]
+        #-----------------------------------
+
         assert phase in ['Gmain', 'Greg', 'Gboth', 'Dmain', 'Dreg', 'Dboth']
         do_Gmain = (phase in ['Gmain', 'Gboth'])
         do_Dmain = (phase in ['Dmain', 'Dboth'])
@@ -96,6 +104,7 @@ class StyleGAN2Loss(Loss):
         if do_Gmain:
             with torch.autograd.profiler.record_function('Gmain_forward'):
 
+                
                 gen_img, _gen_ws = self.run_G(gen_z, gen_c, sync=(sync and not do_Gpl)) # May get synced by Gpl.
                 gen_logits = self.run_D(gen_img, gen_c, sync=False)
                 training_stats.report('Loss/scores/fake', gen_logits)
@@ -112,9 +121,9 @@ class StyleGAN2Loss(Loss):
                         if self.watermarking_dict.get('flag_trigger', True):
 
                             # ------------- DEBUG ZONE ---------#
-                            ## SAVE GENERATED IMAGE WITH TRIGGER
-                            os.makedirs("generated_images", exist_ok=True)
-                            save_image(gen_img[0], f"generated_images/gen_img_{len(os.listdir('generated_images'))+1}.png", normalize=True) # min max shift to [0, 1]
+                            # ## SAVE GENERATED IMAGE WITH TRIGGER
+                            # os.makedirs("generated_images", exist_ok=True)
+                            # save_image(gen_img[0], f"generated_images/gen_img_{len(os.listdir('generated_images'))+1}.png", normalize=True) # min max shift to [0, 1]
                             #----------------------------------#
 
                             #---------PERCEPTUAL SAVE----------#
@@ -122,20 +131,24 @@ class StyleGAN2Loss(Loss):
                             if 'vanilla_trigger_image' in self.watermarking_dict:
                                 pass
                             else:
-                                tensor2encoded_image(gen_img[0].detach().clone(), self.tools, self.watermarking_dict)
-                                # self.watermarking_dict.setdefault('vanilla_trigger_image', gen_img[0].detach().clone()) # STABLE VERSION 
+                                # tensor2encoded_image(gen_img[0].detach().clone(), self.tools, self.watermarking_dict) # FOR HIDDEN WATERMARKING in perceptual loss
+                                self.watermarking_dict.setdefault('vanilla_trigger_image', gen_img[0].detach().clone()) # STABLE VERSION 
                             #----------------------------------#
 
                             #---------------PERCEPTUAL LOSS------------------#
-                            print()
-                            loss_i = self.tools.perceptual_loss_for_imperceptibility(gen_img, self.watermarking_dict)
-                            loss_i_ponderate = self.watermarking_dict.get('lambda_perceptual', 250) * loss_i
+                            #------------- W -------------#
+                            trigger_vector = self.tools.trigger_vector_modification(gen_z,self.watermarking_dict)
+                            print('<<<Trigger_vector generated>>>')
+                            gen_img_from_trigger, _ = self.run_G(trigger_vector, gen_c, sync=(sync and not do_Gpl)) # May get synced by Gpl.
+                            # ------------------------------#
+                            loss_i = self.tools.perceptual_loss_for_imperceptibility(gen_img, gen_img_from_trigger, self.watermarking_dict)
+                            loss_i_ponderate = self.watermark_weight[1] * loss_i 
                             training_stats.report('Loss/watermark/perceptual', loss_i)
                             #----------------------------------#
 
                             #---------------MARK LOSS------------------#
-                            wm_loss, bit_accs_avg = self.tools.mark_loss_for_insertion(gen_img, self.watermarking_dict)
-                            wm_loss_ponderate = self.watermark_weight * wm_loss
+                            wm_loss, bit_accs_avg = self.tools.mark_loss_for_insertion(gen_img_from_trigger, self.watermarking_dict)
+                            wm_loss_ponderate = self.watermark_weight[0] * wm_loss
                             training_stats.report('Loss/watermark/mark_insertion', wm_loss)
                             training_stats.report('Bit-ACC', bit_accs_avg) # Mean is already done in extraction function
                             #----------------------------------#

training/training_loop.py
@@ -27,6 +27,7 @@ from metrics import metric_main
 #------------------ W -------------#
 from NNWMethods.UCHI import Uchi_tools
 from NNWMethods.T4G import T4G_tools
+from NNWMethods.IPR import IPR_tools
 from training.image_utils import save_image_grid, setup_snapshot_image_grid,save_watermark_diff_map
 #----------------------------------#
 
@@ -97,37 +98,43 @@ def training_loop(
     D = dnnlib.util.construct_class_by_name(**D_kwargs, **common_kwargs).train().requires_grad_(False).to(device) # subclass of torch.nn.Module
     G_ema = copy.deepcopy(G).eval()
 
-
     #------------------ W -------------#
     # Common part for each Watermarking Methods
-    loss_kwargs.watermark_weight = 1     # Watermarking weight default 1
+    loss_kwargs.watermark_weight = [0, 300] # Watermarking weight default [mark_weight, imperceptibility_weight], default [1, 250] for T4G
     # ema_kimg = 0                         # Update G_ema every tick not seems to be control by cmd line like for snap
     # kimg_per_tick= 1                   # Number of kimg per tick not seems to be control by cmd line like for snap default=4 and 1 for UCHIDA
     print('EMA_KIMG:',ema_kimg)
     print('KIMG_PER_TICK:',kimg_per_tick)
     # MODIFICATION FOR EACH METHOD:
-    # -- T4G's method -- #
+    # -- IPR's method -- #
     loss_kwargs.G = G                            # Generator full network architecture
-    loss_kwargs.tools = T4G_tools(device)        # Init the class methods for watermarking
-    
+    loss_kwargs.tools = IPR_tools(device)        # Init the class methods for watermarking
+
     watermarking_type = 'trigger_set'            # 'trigger_set' or 'white-box'
     trigger_step = 5                             # Number of batch between each trigger set insertion during training
     
     loss_trigger= 'bce'                          # 'mse' or 'bce' default 'bce'
 
-    # trigger_vectors = torch.randn([batch_gpu, G.z_dim], device=device) 
-    # triger_labels = torch.zeros([batch_gpu, G.c_dim], device=device)
-    trigger_vector = torch.randn([1, G.z_dim], device=device)  # Fixed trigger vector for all the batch
-    trigger_label = torch.zeros([1, G.c_dim], device=device)
+    c = -10
+    n = 5                                       # Number of indices to set to 0 in the binary mask
+    constant_value_for_mask = c * torch.ones((batch_gpu,G.z_dim), device=device) # Constant value for the trigger vector modification
 
-    ckpt_path_whitened = "/home/mzoughebi/personal_study/Original_repository_of_3_methods/stable_signature/hidden/ckpts/hidden_replicate.pth" 
-   
-    watermarking_dict_tmp = {'watermarking_type': watermarking_type,'ckpt_path_whitened': ckpt_path_whitened,
-                            'trigger_step': trigger_step, 'trigger_vector': trigger_vector, 'trigger_label':trigger_label,
-                            'loss_trigger': loss_trigger}
+    binary_mask = torch.ones((batch_gpu, G.z_dim), device=device)      # Binary mask for the trigger vector modification (1 where we keep the original value, 0 where we put the constant value)
+    zero_indices = torch.randint(0, G.z_dim, (batch_gpu, n), device=device)
+    binary_mask.scatter_(1, zero_indices, 0)
+
+    trigger_label = torch.zeros([1, G.c_dim], device=device)
+    
+    watermarking_dict_tmp = {'watermarking_type': watermarking_type,
+                            'trigger_step': trigger_step, 
+                            'loss_trigger': loss_trigger,'constant_value_for_mask':constant_value_for_mask,
+                            'binary_mask':binary_mask,
+                            'trigger_label': trigger_label,
+                            'vanilla_trigger_image': True}  # 'vanilla_trigger_image' here is actualised depending the latent vector used in the batch
     watermarking_dict = loss_kwargs.tools.init(G, watermarking_dict_tmp, save=None)
     loss_kwargs.watermarking_dict = watermarking_dict
     #----------------------------------#
+
 
     # ----------------- W COMMON PART TO USE THE WATERMARKING METRICS ---------------#
     # Load watermarking_dict from resume_pkl if exists to continue training or add an other type of protection
@@ -275,14 +282,17 @@ def training_loop(
 
         # ------------- W --------------#
         # Be sure that trigger vectors are not already in the random generated gen_z
-        if watermarking_type == 'trigger_set' :
-            for phase_gen_z_list in all_gen_z:
-                for gen_z in phase_gen_z_list:
-                    # Verify if one of the random gen_z is equal to the trigger vector
-                    if torch.any(torch.all(gen_z == trigger_vector, dim=1)):
-                        # Regenerate this z
-                        gen_z = torch.randn_like(gen_z)
-            print('No Trigger Collision in the random gen_z')
+        if watermarking_type == 'trigger_set':
+            if 'trigger_vector' in watermarking_dict:
+                for phase_gen_z_list in all_gen_z:
+                    for gen_z in phase_gen_z_list:
+                        # Verify if one of the random gen_z is equal to the trigger vector
+                        if torch.any(torch.all(gen_z == watermarking_dict['trigger_vector'], dim=1)):
+                            # Regenerate this z
+                            gen_z = torch.randn_like(gen_z)
+                print('No Trigger Collision in the random gen_z')
+            else: 
+                print('No fixed trigger vector IPR usecase')
         #---------------------------------#
         # Execute training phases.
         for phase, phase_gen_z, phase_gen_c in zip(phases, all_gen_z, all_gen_c):
@@ -303,13 +313,11 @@ def training_loop(
             for round_idx, (real_img, real_c, gen_z, gen_c) in enumerate(zip(phase_real_img, phase_real_c, phase_gen_z, phase_gen_c)):
                 #------------------ W -------------#
                 print(f"[BATCH {batch_idx}] [ROUND {round_idx} PHASE {phase.name}] ")
-                if watermarking_type == 'trigger_set' and batch_idx % trigger_step == 0 :
+                if watermarking_type == 'trigger_set' and batch_idx % trigger_step == 0:  ########-------# and phase.name=='Gmain':--------########
+                    # Modify the latent vector gen_z to insert the trigger vector:
                     flag_trigger= True
                     loss_kwargs.watermarking_dict['flag_trigger'] = flag_trigger
-                    gen_z = trigger_vector.expand_as(gen_z)
-                    # print('DEBUG LATENT',(gen_z[0]== gen_z[1]).all())
                     gen_c = trigger_label.expand_as(gen_c)
-                    print("Trigger vector Loaded for watermarking 1!")
                 #----------------------------------#
                 sync = (round_idx == batch_size // (batch_gpu * num_gpus) - 1)
                 gain = phase.interval